f43183cf689b5434a5fc8f47d694f584b82c3d94
saketp18 <saketp18@gmail.com>
2020-02-10 20:44:49 +0530

    Handle using asyn await

--- app/src/main/java/com/lite/newsapp/ui/MainViewModel.kt
+++ app/src/main/java/com/lite/newsapp/ui/MainViewModel.kt
@@ -1,6 +1,7 @@
 package com.lite.newsapp.ui
 
 import android.util.Log
+import androidx.databinding.ObservableField
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.lite.newsapp.data.Repository
@@ -15,15 +16,39 @@ class MainViewModel : ViewModel() {
     private val viewModelJob = SupervisorJob()
     private val viewModelScope = CoroutineScope(IO + viewModelJob)
     val mutableNewsResponse = MutableLiveData<NewsResponse>()
-    val progressSate = CustomObservableField(true)
+    val progressSate = ObservableField(true)
 
     fun getNewsResponse(map: Map<String, String>) {
         viewModelScope.launch {
+            Log.d("getNewsResponse", "From IO Context 1: ${Thread.currentThread().name}")
             val response = repository.getNewsData(map)
-            progressSate.set(false)
-            Log.d("getNewsResponse", "From IO Context ${Thread.currentThread().name}")
+            Log.d("getNewsResponse", "From IO Context 2: ${Thread.currentThread().name}")
             withContext(Dispatchers.Main) {
                 if (response.isSuccessful) {
+                    progressSate.set(false)
+                    //Best practice to update UI data variables
+                    //using withContext with Dispatchers.Main, but only
+                    //livedata are there then you can postValue without switching context
+                    //mutableNewsResponse.postValue(response.body())
+                    mutableNewsResponse.value = response.body()
+                    Log.d("getNewsResponse", "From Main Context ${Thread.currentThread().name}")
+                    //mutableNewsResponse.postValue(response.body())
+                } else {
+                    Log.d("NewsApp", "Error ${response.code()} and ${response.errorBody()}")
+                }
+            }
+        }
+    }
+
+    fun getNewsResponses(map: Map<String, String>) {
+        viewModelScope.launch {
+            val result = async {
+                repository.getNewsData(map)
+            }
+            val response = result.await()
+            withContext(Dispatchers.Main) {
+                if (response.isSuccessful) {
+                    progressSate.set(false)
                     //Best practice to update UI data variables
                     //using withContext with Dispatchers.Main, but only
                     //livedata are there then you can postValue without switching context
